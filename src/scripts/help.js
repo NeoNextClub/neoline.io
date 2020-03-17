38d5649e34cb09bb081bd7fae6c8c69bbc045c95
haiwulei <1021391538@qq.com>
2020-03-17 17:23:09 +0800

    fix(help): optimize the code

--- src/scripts/help.js
+++ src/scripts/help.js
@@ -4,11 +4,7 @@ import * as $ from 'jquery';
 
 $(document).ready(function () {
     $('.item-title').on('click', function() {
-        if($(this).children('.down').hasClass('active')) {
-            $(this).children('.down').removeClass('active');
-        } else {
-            $(this).children('.down').addClass('active');
-        }
+        $(this).children('.down').toggleClass('active');
         $(this).parent('.category-item').children('.item-content').toggle();
     })
 });
